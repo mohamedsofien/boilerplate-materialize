51f57abcc4ae582600e0e53f8b3f2e457ac0d133
Sofien KRIAA <kriasofien@gmail.com>
2017-08-26 05:01:51 +0100

    add side and head bars

---
+++ imports/startup/client/ui-plugin.js
@@ -0,0 +1,3 @@
+import '/node_modules/hammerjs/hammer.js';
+import '/node_modules/materialize-css/dist/js/materialize.js';
+import '/node_modules/adminbsb-materialdesign/js/admin.js';
--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -1,1 +1,32 @@
 import './body.html';
+import './head-bar/head-bar.js';
+import './side-bar/side-bar.js';
+import './index.js';
+
+BlazeLayout.setRoot('body');
+let i;
+Template.App_body.onCreated(() => {
+    i = 0;
+    $('body').addClass('theme-blue');
+    /*$('.barss').on('click', function () {
+
+        //if ($('body').hasClass('overlay-open')) { $('.overlay').fadeIn(); } else {$('.overlay').fadeOut(); }
+    });*/
+});
+
+Template.App_body.onRendered(() => {
+    $('body').removeClass('ls-closed');
+});
+
+Template.App_body.events({
+    'click .barss'() {
+        console.log('hellooooo');
+        if (i == 0) {
+            $('body').addClass('overlay-open');
+            i = 1;
+        } else {
+            $('body').removeClass('overlay-open');
+            i = 0;
+        }
+    }
+});
